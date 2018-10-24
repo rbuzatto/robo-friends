f1ed0b9fb51fecabd9412055e6c6b9fe0dcd8cd9
rbuzatto <mightyrafa@gmail.com>
2018-10-24 12:14:18 -0300

    Create App Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import 'tachyons';
-import { robots } from './robots';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <CardList robots={robots} />    
+    <App />    
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
