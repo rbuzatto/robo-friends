fb4d5aa238dba042b28bf95c42b13cdbdb4f598a
rbuzatto <mightyrafa@gmail.com>
2018-10-24 14:57:59 -0300

    Create SearchBox Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => (
-    <CardList robots={robots} />
-);
+class App extends Component {
+    state = {
+        data: robots
+    };
+
+    searchRobots = (e) => {
+        const search = e.target.value.toLowerCase();
+        this.setState(() => ({
+            data: robots.filter(({ name }) => name.toLowerCase().includes(search))
+        }));
+    };
+    
+    render () {
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox search={this.searchRobots} />
+                <CardList robots={this.state.data} />
+            </div>
+        )
+    }
+};
 
 export default App;
