dc41800ca4cf515d8d44e0260d82c62c0c31f638
rbuzatto <mightyrafa@gmail.com>
2018-10-25 20:00:36 -0300

    update searchbox

--- src/App.js
+++ src/App.js
@@ -2,27 +2,43 @@ import React, { Component } from 'react';
 
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
-
+const API = 'https://jsonplaceholder.typicode.com/users';
 class App extends Component {
     state = {
-        data: robots
+        data: [],
+        filter: ''
+    };
+
+    componentDidMount() {
+        fetch(API)
+            .then(res => {
+                if (res.status === 200) return res.json(); 
+                throw res.status
+            })
+            .then(data => this.setState(() => ({ data })))
+            .catch(console.log)
+    }
+
+    filterRobots = (robots, filter) => {
+        return robots.filter(({ name }) => name.toLowerCase().includes(filter))
     };
 
     searchRobots = (e) => {
         const search = e.target.value.toLowerCase();
         this.setState(() => ({
-            data: robots.filter(({ name }) => name.toLowerCase().includes(search))
+            filter: search
         }));
     };
     
     render () {
-        return (
+        const {data, filter} = this.state;
+
+        return data.length === 0 ? <h1 className="tc">Loading...</h1> : (
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
                 <SearchBox search={this.searchRobots} />
-                <CardList robots={this.state.data} />
+                <CardList robots={this.filterRobots(data, filter)} />
             </div>
         )
     }
