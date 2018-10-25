7fbedbc1e1efef195a8e3fe197e3dc9e78fcc89c
rbuzatto <mightyrafa@gmail.com>
2018-10-25 18:44:26 -0300

    Add Card test case

---
+++ src/tests/Card.test.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+jest.setTimeout(100000);
+let page, browser;
+
+beforeEach( async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000');
+    }
+);
+
+afterEach( async () => {
+    await browser.close();
+    }
+);
+
+test('text graber', async () => {
+    const text = await page.$eval('div h2', el => el.innerHTML);
+    expect(text).toMatch('Frank Sinatra');
+});
+
